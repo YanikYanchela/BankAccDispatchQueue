1529d839f406657276d5a807c7456471d1e56399
YanikYanchela <yanik-1993@rambler.ru>
2024-03-24 21:08:47 +0300

    dispatchQuene

---
+++ BankAcc/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  BankAcc
+//
+//  Created by Дмитрий Яновский on 17.03.24.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    let account = BankAccount()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        account.test()
+        
+    }
+}
+
+
+
